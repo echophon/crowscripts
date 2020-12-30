de58d91aebd19b21962e4b7afdf99b6f64a4ad11
echophon <yancyway@gmail.com>
2020-12-30 13:01:25 -0800

    initial commit

---
+++ vcswitch.lua
@@ -0,0 +1,31 @@
+-- voltage controlled switch 
+-- in1: select output based on 0-5v value
+-- in2: cv input
+-- out1-4: voltage from in2 (when selected)
+
+
+function init()
+    -- set window ranges of 1.25v to choose outputs
+    input[1].mode( 'window', {0,1.25,2.5,3.75,5}, 0.1 )
+    input[2].mode( 'stream', 0.01)
+end
+
+dest = 1
+
+input[1].window = function(state)
+    dest = state
+
+    for n=1,4 do
+        if n == state then
+        -- do nothing, maintain the current cv
+        else
+        -- reset to zero
+            output[n].slew =0.001
+            output[n].volts=0
+        end
+    end
+end
+
+input[2].stream = function(v)
+    output[(dest%4)+1].volts = v
+end    
