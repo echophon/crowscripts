5f7a19b3a8739629ffd399507409232fbe275d89
echophon <yancyway@gmail.com>
2020-12-30 13:07:59 -0800

    cleanup

--- vcswitch.lua
+++ vcswitch.lua
@@ -8,6 +8,10 @@ function init()
     -- set window ranges of 1.25v to choose outputs
     input[1].mode( 'window', {0,1.25,2.5,3.75,5}, 0.1 )
     input[2].mode( 'stream', 0.01)
+    for n=1,4 do
+        output[n].slew=0
+        output[n].volts=0
+    end
 end
 
 dest = 1
@@ -20,7 +24,6 @@ input[1].window = function(state)
         -- do nothing, maintain the current cv
         else
         -- reset to zero
-            output[n].slew =0.001
             output[n].volts=0
         end
     end
